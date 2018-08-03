d6076531f8170e47f92afb11dfed0beda3f3ae09
james willett <30274748+jameswillett@users.noreply.github.com>
2018-08-03 00:08:48 -0400

    remove log

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Promise = require('bluebird');
 const app = express();
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://james:@localhost:5432/cokebust';
-console.log(connectionString);
 const pool = new Pool({ connectionString });
 
 app.use((req, res, next) => {
