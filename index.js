a19af9ed1be288ad4be4537978419f54e24492a5
James Willett <jms.wlltt@gmail.com>
2019-08-14 15:41:31 -0400

    lemme see something

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 const makeGillissLifeHarder = (req, res, next) => {
   console.log(req.headers.origin);
-  console.log(req.headers.host);
+  console.log(NODE_ENV);
   next();
 }
 
